dd013b5115542d753997c439b391f117bbebba52
renan-goes <36930305+Renan-Goes@users.noreply.github.com>
2020-11-14 01:24:19 -0300

    added generate graph specific ips

--- data_reader.py
+++ data_reader.py
@@ -72,7 +72,33 @@ def data_visualization_server_client(data):
     ax2.bar(dest_ips, num_dest_ips)
     plt.savefig('destination_ips.png')
 
-def data_visualization_flow_rate(data):
+def data_visualization_flow_rate_one(data, user_ip, service_ip):
+    times_found_download = []
+    times_found_upload = []
+    sizes_found_download = []
+    sizes_found_upload = []
+
+    for index in range(len(data['time'])):
+            if data['ip_to_ip'][index] == (user_ip + '->' + service_ip):
+                times_found_download.append(int(float(data['time'][index])))
+                sizes_found_download.append(int(float(data['length'][index])))
+
+            elif data['ip_to_ip'][index] == (service_ip + '->' + user_ip):
+                times_found_upload.append(int(float(data['time'][index])))
+                sizes_found_upload.append(int(float(data['length'][index])))
+
+    fig = plt.figure(figsize=(15, 5))
+    ax = fig.add_subplot(111)
+
+    plt.plot(times_found_download, sizes_found_download, label='Download')
+    plt.plot(times_found_upload, sizes_found_upload, label='Upload')
+
+    plt.legend(loc='center', bbox_to_anchor=(-0.1, 0.4))
+
+    plt.savefig('flow_rate_specific.png')
+
+    
+def data_visualization_flow_rate_all(data):
     dict_ip_to_ip_original = Counter(data['ip_to_ip'])
     dict_ip_to_ip = {k: v for k, v in dict_ip_to_ip_original.items() if v >= 100}
     
@@ -102,9 +128,11 @@ def data_visualization_flow_rate(data):
     
 data = get_data('capture.csv')
 print('\nData has been read!')
-# data_visualization_protocols(data)
-# print('Protocol pie chart generated...')
-# data_visualization_server_client(data)
-# print('Server and client bar chart generated...')
-data_visualization_flow_rate(data)
-print('Flow rate graph generated...\n')
+data_visualization_protocols(data)
+print('Protocol pie chart generated...')
+data_visualization_server_client(data)
+print('Server and client bar chart generated...')
+data_visualization_flow_rate_all(data)
+print('Flow rate graph generated...')
+data_visualization_flow_rate_one(data, '104.237.167.26', '192.168.0.112')
+print('Flow rate graph for specific ip\'s generated...\n')
